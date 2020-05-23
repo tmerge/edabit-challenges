2ecf02a180c0dce3f6aa258adfbb2bca70ab98b7
tmerge <tmerge@uos.de>
2020-05-23 21:03:17 +0200

    complete task6

---
+++ task6/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+/* TASK: Create a function that takes a strings characters as ASCII and returns each characters hexadecimal value as a string. */
+
+// ConvertToHex take a string and convert it to hex values then print it
+func ConvertToHex(input string) {
+	inputAsArray := []byte(input)
+
+	// Print hex values with whitespaces
+	fmt.Print(input + ": ")
+	for i := 0; i < len(inputAsArray); i++ {
+		fmt.Printf("%X ", inputAsArray[i])
+	}
+	// Print break line to prettfy output
+	fmt.Print("\n")
+}
+
+func main() {
+
+	// Test ConvertToHex with "hello world" -> Expect: 68 65 6c 6c 6f 20 77 6f 72 6c 64
+	ConvertToHex("hello world")
+
+	// Test ConvertToHex with "Welcome" -> Expect:
+	ConvertToHex("Welcome")
+
+}
